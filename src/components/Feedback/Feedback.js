e9ed503caa9b7221064a9f4f3f296be47a980c9f
RhondaStark92 <rhondastark@comcast.net>
2018-11-05 08:32:12 -0600

    not sure what i changed .. gave up

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,44 +1,86 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
+
+const emptyFeedback = {
+    feeling: 0,
+    understanding: 0,
+    support: 0,
+    comments: ''
+}
 
 class Feedback extends Component {
 
-    state = {
-            feeling: 0,
-            understanding: 0,
-            support: 0,
-            comments: ''
-    }
+    state = emptyFeedback;
 
     componentDidMount() {
         // Do I need to do anything here?
-        console.log('in feedback', this.props.match.params.id);
+        console.log('in feedback');
+        
     }
 
     //takes answer from input and set in state
     handleChange = (event) => {
+        // console.log('in handle change');
+        // console.log('state before setState', event);
         this.setState({
-          ...this.state,
-          feeling: event.target.value
-        })
+            [event.target.name]: event.target.value,
+        });
+        // console.log('state after', this.state);
     }
+
+    // handleChangeFeeling = (event) => {
+    //     console.log('feeling', event.target.value);
+    //     this.setState({
+    //         ...this.state,
+    //         feeling: event.target.value
+    //     })
+    // }
+
+    // handleChangeUnderstanding = (event) => {
+    //     this.setState({
+    //         ...this.state,
+    //         understanding: event.target.value
+    //     })
+    // }
+
+    // handleChangeSupport = (event) => {
+    //     this.setState({
+    //         ...this.state,
+    //         support: event.target.value
+    //     })
+    // }
+
+    // handleChangeComments = (event) => {
+    //     this.setState({
+    //         ...this.state,
+    //         comments: event.target.value
+    //     })
+    // }
 
     handleNext = (event) => {
         // Send the local state of the entry to Redux 
         // to save for the final submit button
         event.preventDefault();
-        console.log('in click next button', event.target.value);
+        // console.log('in click next button', event.target.value);
         this.props.history.push(event.target.value);
     }
 
  
     handleSubmit = (event)  => {
-        // Send the local state of the entry to Redux 
-        // to save for the final submit button
         event.preventDefault();
-        console.log('in click submit button', event.target.value);
-        this.props.history.push(event.target.value);
+        let nextPath = '';
+        console.log('in click submit button', this.state, this.props.match.params.id);
+        if (typeof this.props.match.params.id === "undefined") {
+            // first time in set pageNumber to 1
+            nextPath = '/2';
+        } else if (this.props.match.params.id == 5) {
+            nextPath = '/';
+            console.log('in last line');
+            this.setState({emptyFeedback});
+        } else {
+            nextPath = '/' + (parseInt(this.props.match.params.id)+1);
+        }
+        this.props.history.push(nextPath);
         // Update the page number information
         // this.props.dispatch({ type: 'GET_PAGENUMBER', payload: this.props.match.params.id});
     }
@@ -54,7 +96,8 @@ class Feedback extends Component {
         let nextPath;
         let button;
         let inputSection;
-
+        let inputButton;
+        console.log('check id', this.props.match.params.id);
         if (typeof this.props.match.params.id === "undefined") {
             // first time in set pageNumber to 1
             pageNumber = 1;
@@ -63,20 +106,39 @@ class Feedback extends Component {
         }
         
         nextPath = '/' + (pageNumber + 1);
-        button = <button value={nextPath} onClick={this.handleNext}>Next</button>;
+        // button = <button value={nextPath} 
+        //     onClick={this.handleNext}>Next</button>;
+        inputButton = <input type="submit" value="Next" name={nextPath} />
         let pageHeader = <p>Page {pageNumber} of 4.</p>
-        inputSection = <input placeholder="Enter 1 to 5" name ="inputEntry"></input>
 
         switch (pageNumber) {
-           case 4:
-                inputSection = <input placeholder="Your message here." name ="inputEntry"></input>
-                button = <button value={nextPath} onClick={this.handleSubmit}>Submit</button>
+            case 1:
+                inputSection = <input onChange={this.handleChange}
+                    placeholder="Enter 1 to 5" name="feeling"></input>
+                break;
+            case 2:
+                inputSection = <input onChange={this.handleChange}  
+                    placeholder="Enter 1 to 5" value={this.state.understanding} name ="understanding"></input>
+                break;
+            case 3:
+                inputSection = <input onChange={this.handleChange} 
+                    placeholder="Enter 1 to 5" value={this.state.support} name ="support"></input>
+                break;
+            case 4:
+                inputSection = <input onChange={this.handleChange} 
+                    placeholder="Your message here." value={this.state.comments} name ="comments"></input>
+                // button = <button value={nextPath} 
+                //     onClick={this.handleSubmit}>Submit</button>
+                inputButton = <input type="submit" value="Submit" name={nextPath} />
                 break;
             case 5:
                 nextPath = '/';
                 inputSection = '';
                 pageHeader = <h2>Thanks for the feedback!</h2>;
-                button = <button value={nextPath} onClick={this.handleNext}>New Feedback</button>
+                inputButton = <input type="submit" value="New Feedback" name={nextPath} />
+                // button = <button value={nextPath} 
+                //     onClick={this.handleNext}>New Feedback</button>;
+                break;
             default:
         }
         // refresh the DOM
@@ -84,14 +146,16 @@ class Feedback extends Component {
             <div>
                 {pageHeader}
                 <h2>{questions[pageNumber - 1]}</h2>
-                {inputSection}
-                {button}
+                <form onSubmit={this.handleSubmit}>
+                    {inputSection}
+                    {inputButton}
+                </form>
+                {/* {button} */}
             </div>
         );
     }
 }
 
 const mapStateToProps = reduxState => ({ reduxState });
-
 export default connect(mapStateToProps)(Feedback);
 
--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // Create reducer
-const feedbackReducer = (state = [], action) => {
+const feedbackReducer = (state = {}, action) => {
     console.log('In reducer');
     if ( action.type  === 'SET_FEEDBACK' ) {
       // action payload should be the array from the server
       return action.payload;
     }
-    if ( action.type  === 'GET_FEEDBACK' ) {
+    if ( action.type  === 'SET_FEELING' ) {
+        
         // action payload should be the array of feedback from the server
         return action.payload;
     }
